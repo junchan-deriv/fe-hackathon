c02b58dbb04f853c4cf78d62a17b3a44822ece41
Vern <vern.yi@besquare.com.my>
2022-07-19 11:03:33 +0800

    added files for home, about and market

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Main from "./pages/main";
+import Header from "./pages/header/header";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +12,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<nav />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
